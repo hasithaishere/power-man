f4ba6fe8a784a5c5a9e5fb112f8ee8e2aeab8484
hasitha.hpmax <hasitha.hpmax@cf12d2a9-c71b-8214-24f5-556040d79573>
2013-07-14 20:41:36 +0000

    2013-07-15-Morning 2-Hasitha - CSS and Design Changes

---
+++ trunk/Application/powerman/application/controllers/confirm_user.php
@@ -0,0 +1,9 @@
+<?php
+
+class confirm_user extends CI_Controller
+{
+	function index()
+	{
+		$this->load->view('confirm_user');
+	}
+}
--- trunk/Application/powerman/application/views/confirm_user.php
+++ trunk/Application/powerman/application/views/confirm_user.php
@@ -58,34 +58,28 @@
 						<a href="index.html"><i class="icon-home"></i></a>
 						<a href="#"><i class="icon-cog"></i></a>
 					</div>
-					<h2>Login to your account</h2>
+					<h1>User Confirmation</h1>
 					<form class="form-horizontal" action="<?php echo base_url();?>main_panel" method="post">
 						<fieldset>
 							
 							<div class="input-prepend" title="Username">
-								<span class="add-on"><i class="icon-user"></i></span>
-								<input class="input-large span10" name="username" id="username" type="text" placeholder="type username"/>
+								<img class="img-rounded" data-src="holder.js/140x140" alt="140x140" style="width: 400px; height: 140px;" src="<?php echo base_url();?>img/confirmprocess.png">
 							</div>
 							<div class="clearfix"></div>
-
-							<div class="input-prepend" title="Password">
-								<span class="add-on"><i class="icon-lock"></i></span>
-								<input class="input-large span10" name="password" id="password" type="password" placeholder="type password"/>
+							<p>You have to complete this user validation. If you will complete these three steps sucessfully, Our Domore PowerMan system automatically activate you as a valid customer. Please follow the given instruction carefully. Press Start Over button to begin. </p>
+							<div class="clearfix"></div>
+							
+							
+							<hr>
+							<div class="button-login2">	
+								<button type="submit" class="btn btn-primary">Start Over</button>
 							</div>
 							<div class="clearfix"></div>
 							
-							<label class="remember" for="remember"><input type="checkbox" id="remember" />Remember me</label>
-
-							<div class="button-login">	
-								<button type="submit" class="btn btn-primary"><i class="icon-off icon-white"></i> Login</button>
-							</div>
-							<div class="clearfix"></div>
+							
+							</fieldset>
 					</form>
-					<hr>
-					<h3>Forgot Password?</h3>
-					<p>
-						No problem, <a href="#">click here</a> to get a new password.
-					</p>	
+						
 				</div><!--/span-->
 			</div><!--/row-->
 			
