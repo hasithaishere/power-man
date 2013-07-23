9ab920e3e1f980cef6780f2a5f3c669e25f5955a
hasitha.hpmax <hasitha.hpmax@cf12d2a9-c71b-8214-24f5-556040d79573>
2013-07-23 06:21:28 +0000

    2013-07-23-Morning 1-Hasitha - Untitled Update

--- Application/powerman/application/models/confirm_user_model.php
+++ Application/powerman/application/models/confirm_user_model.php
@@ -5,7 +5,7 @@ class confirm_user_model extends CI_Model
 	function update_password($data)
 	{
 		//$this->db->update('power_users',$data,//$this->session->userdata('user_id'));
-		$this->db->where('id', 1);
+		$this->db->where('id', 2);
 		
 		$this->db->update('power_users', $data); 
 	}
