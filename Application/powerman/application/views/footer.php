2b386c5ad5d8fd5a8581670a7744c1414b937eab
harshana071 <harshana071@gmail.com>
2013-07-30 08:52:43 +0000

    2013-07-30 evening-harshana create include files

---
+++ Application/powerman/application/views/footer.php
@@ -0,0 +1,7 @@
+<footer>
+			<p>
+				<span style="text-align:left;float:left">&copy; <a href="#" target="_blank">Power Man</a> 2013</span>
+				<span style="text-align:right;float:right">Powered by: <a href="www.domore.lk">Domore Technologies (Pvt) Limited</a></span>
+			</p>
+
+</footer>
