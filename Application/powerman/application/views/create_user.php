54c8f5ab98235e1a11350c4a2f4c7a7b8664e049
sthiyamba <SThiyamba@gmail.com>
2013-07-31 06:10:44 +0000

    2013-07-31 morning-supun adding user setting page

--- Application/powerman/application/views/create_user.php
+++ Application/powerman/application/views/create_user.php
@@ -37,37 +37,9 @@
 				
 			</div>
 			  <hr>
-                        <div class="sortable row-fluid">
-
-				<a class="quick-button span2">
-					<i class="fa-icon-group"></i>
-					<p>Users</p>
-					<span class="notification">1.367</span>
-				</a>
-				<a class="quick-button span2">
-					<i class="fa-icon-comments-alt"></i>
-					<p>Comments</p>
-					<span class="notification green">167</span>
-				</a>
-				<a class="quick-button span2">
-					<i class="fa-icon-shopping-cart"></i>
-					<p>Orders</p>
-				</a>
-				<a class="quick-button span2">
-					<i class="fa-icon-barcode"></i>
-					<p>Products</p>
-				</a>
-				<a class="quick-button span2">
-					<i class="fa-icon-envelope"></i>
-					<p>Messages</p>
-				</a>
-				<a class="quick-button span2">
-					<i class="fa-icon-calendar"></i>
-					<p>Calendar</p>
-					<span class="notification red">68</span>
-				</a>
-
-			</div>
+                        <!-- start: Quick button Menu -->
+            <?php include 'quick_buttons.php'; ?>
+			<!-- end: Quick button Menu -->
                         <hr>
 			
                      
