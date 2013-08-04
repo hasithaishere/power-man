650176dfc230f26f768c7a58e79c9efcbd9a6ac0
sthiyamba@gmail.com <sthiyamba@gmail.com@cf12d2a9-c71b-8214-24f5-556040d79573>
2013-08-04 13:09:59 +0000

    2013-08-04 evening-supun Email HTML Template

--- trunk/Application/powerman/application/views/Upgrade_Package.php
+++ trunk/Application/powerman/application/views/Upgrade_Package.php
@@ -41,67 +41,53 @@
                      
 			<div class="row-fluid">
 
-        <h1>Upgrade Package</h1></br>
-<form id="form1" name="form1" method="post" action="">
 
-  <div>
-  	<label><h4>Current Package</h4></label>
-  	<select name="select" id="select">
-  	  <option>Home Edition</option>
-  	  <option>Small Business Edition</option>
-  	  <option>Enterprise Edition</option>
-    </select>  	
-  	<span id="CPackage_Details">What's your current package name?</span>  	
-  </div></br>
-    
-   <div>
-  	<label><h4>Serial No</h4></label>
-  	<input type="text" name="ser_no" id="ser_no" />  	
-  	<span id="CPackage_Details">What's your current packages' serial number?</span>  	
-  </div></br>
-  
-  <div> 
-    <label><h4>Description</h4></label>
-    <textarea name="Description" id="Description2" cols="45" rows="5" tabindex="1"></textarea>
-    <span id="Description">Reason for changing package</span>  	</label>
-    
-  </div></br>
-  
-  <div>
-  	<label><h4>New Package</h4></label>
-  	<select name="New_package" id="New_package" tabindex="1">
-  	  <option>Home Edition</option>
-  	  <option>Small Business Edition</option>
-  	  <option>Enterprise Edition</option>
-    </select>    
-  	<span id="NPackage_Details">Request package name?</span>  
-  </div></br>
-    
-  <div>
-    <label><h4>Sub Devices</h4></label>
-    <select name="Sub_dev" id="Sub_dev2" tabindex="1">
-      <option>2</option>
-      <option>1</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-      <option>6</option>
-      <option>7</option>
-      <option>8</option>
-      <option>9</option>
-      <option>10</option>
-    </select>    
-    <span id="Sub_dev">How many sub devices you want to add?</span>  
-  </div></br>
-    
- 	<div>
- 	  <label>
- 	  <input type="submit" name="Upgarde" id="Upgarde" value="Upgrade" tabindex="1" />
- 	  </label>
-  	</div>
-    
-</form>
-<label></label>
+<div id="container">
+	<h1>Request for Upgrade Package</h1>
+    	<form action="" method="post" id="upgrade_form">
+        	<div>
+                <label for="cpackage">Current Package</label>
+                <select name="pack">
+                    <option value="empty" selected></option>
+                <option value="home">Home Edition</option>
+                <option value="business">Small Business Edition</option>
+                <option value="company" >Company Edition</option>
+
+                </select>
+                <span id="CPackage_Details">What's your current package name?</span>
+            </div>
+             
+             <div>
+            	<label for="s_number">Serial No </label>
+                <input id="s_number" name="s_number" type="text" />
+                <span id="s_number_Details">What's your current packages' serial number?</span>  
+                
+             </div>
+             
+              <div>
+            	<label for="description">Description </label>
+                <textarea name="description" id="description" cols="45" rows="5" tabindex="1"></textarea>
+                <span id="descriptionDetails">Reason for changing package</span>
+             </div>
+             
+             <div>
+                <label for="npackage">New Package Type</label>
+                <select name="pack">
+                    <option value="empty" selected></option>
+                <option value="home">Home Edition</option>
+                <option value="business">Small Business Edition</option>
+                <option value="company" >Company Edition</option>
+
+                </select>
+                <span id="NPackage_Details">What's your New package name?</span>
+            </div>
+             
+             <div>
+             	<input id="upgrade" name="upgrade" type="submit" value="Upgrade" />
+             </div>
+             </form>
+
+</div>
 			<hr>
 			
 			
