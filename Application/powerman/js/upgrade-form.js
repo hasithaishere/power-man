827b9f77ba4348af5a4bb6b26ce2497911d2d281
kosalakoshi88 <kosalakoshi88@gmail.com>
2013-08-09 14:27:51 +0000

    2013-08-09,evening,koshala,validate upgrade package form

--- Application/powerman/js/upgrade-form.js
+++ Application/powerman/js/upgrade-form.js
@@ -1,29 +1,74 @@
 $(document).ready(function() {
 //	alert("a");
     var upgrade_form = $("#upgrade_form");
+	var s_number = $("#s_number");
+	var s_number_Details = $("#s_number_Details");
 	var description = $("#description");
 	var descriptionDetails = $("#descriptionDetails");
-
+	var message = $("#message");
 	
+	s_number.blur(validateSerialnumber);
 	description.blur(validateDescription);
+	
+	s_number.keyup(validateSerialnumber);
 	description.keyup(validateDescription);
-
+	
 	upgrade_form.submit(function(){
-			if(validateDescription() & Serialnumber()){
-			return true;
-			}else{
+			if(validateDescription() & validateSerialnumber() & validatemessage())
+			{
+				return true;
+			}
+			else
+			{
 				return false;
 			}
-		});
+		
+	});
 	
 
-	function validateDescription(){
-		if(description.val().length<10){
+
+	function validateSerialnumber(){
+		
+		$(function(){
+			$('#s_number').keydown(function(e) {
+				if (e.shiftKey || e.ctrlKey || e.altKey) 
+				{
+					e.preventDefault();
+					
+				} 
+				else 
+				{
+					var key = e.keyCode;
+					if (!((key == 8) || (key == 46) || (key >= 35 && key <= 40) || (key >= 48 && key <= 57) || (key >= 96 && key <= 105))) 		
+					{
+					alert ("Enter only numbers");	
+					e.preventDefault();
+					
+					}
+				
+				}
+				
+				})
+			
+			})
+		
+				
+	}
+
+	//$('#upgrade').attr("disabled", true);
+	
+
+function validateDescription(){
+		if(description.val().length<25)
+		{
 			description.addClass("error");
-			descriptionDetails.text("Your description is too short. Make it 10 characters or more.");
+			descriptionDetails.text("Your description is too short. Make it 25 characters or more.");
 			descriptionDetails.addClass("error");
 			return false;
-		}else{
+			
+		}
+		else
+		{
 			description.removeClass("error");
 			descriptionDetails.text("Reason for changing package");
 			descriptionDetails.removeClass("error");
@@ -32,57 +77,18 @@ $(document).ready(function() {
 		}
 	}
 	
+	var $input = $('input:text');
+    $upgrade = $('#upgrade');    
+	$upgrade.attr('disabled', true);
 
-		function validateSerialnumber(){
-		
-		$(function(){
-			$('#zipcode').keydown(function(e) {
-				if (e.shiftKey || e.ctrlKey || e.altKey) {
-					e.preventDefault();
-					
-				} else {
-					var key = e.keyCode;
-					if (!((key == 8) || (key == 46) || (key >= 35 && key <= 40) || (key >= 48 && key <= 57) || (key >= 96 && key <= 105))) 			{
-					e.preventDefault();
-				}
-				}
-
-				})
-			})
-		
-				
-				
-}
-		
-		//if (zipcode.val().length<5){
-			//zipcode.addClass("error");
-			//zipcodeDetails.text("Enter 5 numbers only");
-			//zipcodeDetails.addClass("error");
-			//return true;
-		//else{
-			//zipcode.removeClass("error");
-			//zipcodeDetails.text("What's the zip code of your province?");
-			//zipcodeDetails.removeClass("error");
-			//return true;
-		//}
-	//}
-	
-	
-	
-	// $('#send').attr("disabled", true);
-	
-	var $input = $('input:text'),
-    $send = $('#upgrade');    
-	$send.attr('disabled', true);
-
-	$input.keyup(function() {
+	$input.keyup(function(){
     var trigger = false;
     $input.each(function() {
-        if (!$(this).val()) {
+        if (!$(this).val()) 
+		{
             trigger = true;
         }
-    });
-    trigger ? $send.attr('disabled', true) : $send.removeAttr('disabled');
-});
-
-});
+	});
+    trigger ? $upgrade.attr('disabled', true) : $upgrade.removeAttr('disabled');
+});					   
+	});
