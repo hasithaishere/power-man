161b336aed1179f9f1a0123ced6761b4517ef88f
hasitha.hpmax <hasitha.hpmax@gmail.com>
2013-08-11 11:07:22 +0000

    2013-08-11 - Hasitha  Evening3 - Create User and Send registration mail to client Modification (Completed) Part 3

--- Application/powerman/application/libraries/sms_sender.php
+++ Application/powerman/application/libraries/sms_sender.php
@@ -32,27 +32,5 @@ class sms_sender {
  */
  		return TRUE;
     }
-	
-	public function send_mail($data)
-	{
-		//print_r($data);
-		//$this->load->library('email',$config);
-		$CI = &get_instance();
-		$CI->email->set_newline("\r\n");
-		//$data = array('from'=>'ruslpowerman@gmail.com','to'=>'hasitha.hpmax@gmail.com','subject'=>'test mail gfgfdhhgd','message'=>'hi hasitha fgfdgfgs');
-		
-		$CI->email->from($data['from']);
-		$CI->email->to($data['to']);
-		$CI->email->subject($data['subject']);
-		$CI->email->message($data['message']);
-		
-		if($CI->email->send())
-		{
-			echo "send it";
-		}
-		else 
-		{
-			show_error($CI->email->print_debugger());	
-		}
-	}
+
 }
