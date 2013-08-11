3ec21675206caf217e4644f60df8d08f87d8ff9a
hasitha.hpmax@gmail.com <hasitha.hpmax@gmail.com@cf12d2a9-c71b-8214-24f5-556040d79573>
2013-08-11 11:01:37 +0000

    2013-08-11 - Hasitha  Evening1 - Create User and Send registration mail to client Modification (Completed)

--- trunk/Application/powerman/application/controllers/device_on_off.php
+++ trunk/Application/powerman/application/controllers/device_on_off.php
@@ -4,7 +4,35 @@ class device_on_off extends CI_Controller
 {
 	function index()
 	{
-		$this->load->view('device_on_off');
+		//$CI =& get_instance();
+		
+		//$this->load->helper('randomgenerator');
+		//sendemail(array('from'=>'ruslpowerman@gmail.com','to'=>'hasitha.hpmax@gmail.com','subject'=>'test mail 1','message'=>'hi hasitha'));
+		
+					$this->load->library('email_sender');
+					
+					$data1 = array(
+						'email' =>  'hasitha@gmail.com',
+						'password' => 'fds54DSgrw',
+						'email_code' => 'sfgst45fsw54y65'
+					);
+					
+					$message=$this->load->view('email_template',$data1,TRUE);
+					$data = array('from'=>'ruslpowerman@gmail.com','sender'=>'Power Man Admin','to'=>'hasitha.hpmax@gmail.com','receiver'=>'Hasitha Prabhath Gamage','subject'=>'test mail 1','message'=>$message);
+					
+					$result2 = $this->email_sender->send_email($data);
+					
+					if($result2)
+					{
+						echo "Ohh Yheee";
+					}
+			
+		//die(randomPassword()."====".randomPhoneCode());
+		//$this->load->view('device_on_off');
+
+		
+		
+		
 	}
 	
 	
---
+++ trunk/Application/powerman/application/models/create_user_model.php
@@ -0,0 +1,55 @@
+<?php
+
+class create_user_model extends CI_Model
+{
+	function add_user()
+	{
+		$this->load->helper('randomgenerator');
+		
+		$email_code_val = randomEmailCode();
+		$phone_code_val = randomPhoneCode();
+		
+		$user_data = array(
+			'fname' => $this->input->post('fname'),
+			'lname' => $this->input->post('lname'),
+			'address1' => $this->input->post('address1'),
+			'address2' => $this->input->post('address2'),
+			'city' => $this->input->post('city'),
+			'province' => $this->input->post('province'),
+			'zip' => $this->input->post('zipcode'),
+			'country' => $this->input->post('countries'),
+			'email' => $this->input->post('email'),
+			'package' => $this->input->post('pack'),
+			'password' => md5($this->input->post('pass1')),
+			'email_code' => $email_code_val,
+			'phone_code' => $phone_code_val
+		);
+		
+		$return_val = $this->db->insert('power_users',$user_data);
+		
+		if($return_val)
+		{
+					$this->load->library('email_sender');
+					
+					$data1 = array(
+						'email' =>  $this->input->post('email'),
+						'password' => $this->input->post('pass1'),
+						'email_code' => $email_code_val
+					);
+					
+					$message=$this->load->view('email_template',$data1,TRUE);
+					$data = array('from'=>$this->config->item('admin_email'),'sender'=>$this->config->item('admin_email_name'),'to'=>$this->input->post('email'),'receiver'=>$this->input->post('fname') . " " . $this->input->post('lname'),'subject'=>'Registration Details - Domore Power Man','message'=>$message);
+					
+					$result2 = $this->email_sender->send_email($data);
+					
+					if($result2)
+					{
+						$return_val = $result2;
+					}
+					
+			return $return_val;
+		}
+		
+		
+	}
+}
