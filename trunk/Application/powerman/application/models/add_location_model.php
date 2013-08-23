e6088b25054dbe1c823ae324b5d712ead850d58e
sthiyamba@gmail.com <sthiyamba@gmail.com@cf12d2a9-c71b-8214-24f5-556040d79573>
2013-08-23 16:41:02 +0000

    2013-08-23 eveing-supun add_location page db connection

---
+++ trunk/Application/powerman/application/models/add_location_model.php
@@ -0,0 +1,25 @@
+<?php
+
+class add_location_model extends CI_Model
+{
+	function add_location()
+	{
+		
+		
+		$location_data = array(
+			'name' => $this->input->post('location_name'),
+			'sub_name' => $this->input->post('sub_name'),
+			'description' => $this->input->post('location_description'),
+			//'image_url' => 'user.jpg',
+			'image_url' => $this->$data['file_name'],
+			'user_id' => '2'
+			 
+		);
+		
+		$return_val = $this->db->insert('power_location',$location_data);
+		
+		
+		
+		
+	}
+}
