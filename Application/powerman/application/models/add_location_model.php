736ef19a066521aee24706535434c82d1eb29834
sthiyamba <SThiyamba@gmail.com>
2013-08-23 16:49:47 +0000

    2013-08-23 eveing-supun add_location modal

--- Application/powerman/application/models/add_location_model.php
+++ Application/powerman/application/models/add_location_model.php
@@ -10,8 +10,8 @@ class add_location_model extends CI_Model
 			'name' => $this->input->post('location_name'),
 			'sub_name' => $this->input->post('sub_name'),
 			'description' => $this->input->post('location_description'),
-			//'image_url' => 'user.jpg',
-			'image_url' => $this->$data['file_name'],
+			'image_url' => 'user.jpg',
+			//'image_url' => $this->$data['file_name'],
 			'user_id' => '2'
 			 
 		);
