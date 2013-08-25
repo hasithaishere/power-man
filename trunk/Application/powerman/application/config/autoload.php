845dee8f522e077acb6b43a83d80251e312e9809
hasitha.hpmax@gmail.com <hasitha.hpmax@gmail.com@cf12d2a9-c71b-8214-24f5-556040d79573>
2013-08-25 08:36:50 +0000

    2013-08-25 - Hasitha  Evening 1- Fix SVN Conflict  - Part 1

--- trunk/Application/powerman/application/config/autoload.php
+++ trunk/Application/powerman/application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database','session','email','nusoap_base','encrypt');
+$autoload['libraries'] = array('database','session','email','nusoap_base','encrypt_data');
 
 
 /*
