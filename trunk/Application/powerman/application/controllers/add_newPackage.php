f58c5e4b55221d47156247bee300a95e977c7a1b
mksnisrina@gmail.com <mksnisrina@gmail.com@cf12d2a9-c71b-8214-24f5-556040d79573>
2013-09-01 09:09:04 +0000

    2013-08-31-evaning-add_newPackage.php_new-nisrina

---
+++ trunk/Application/powerman/application/controllers/add_newPackage.php
@@ -0,0 +1,18 @@
+<?php
+
+class add_newPackage extends CI_Controller
+{
+	function index()
+	{
+		if(!($this->session->userdata('is_logged_in')))
+		{
+			$this->load->view('access_denied');
+		}
+		else 
+		{
+			$this->load->view('add_newPackage');
+		}
+	}
+	
+	
+}
