b18fabe6f8e6dba24c5f4a767ab1b0cc2d08089b
mksnisrina <mksnisrina@gmail.com>
2013-09-02 08:50:21 +0000

    2013-09-02-evening-add_newPackage_database_connected-nisrina

--- Application/powerman/application/controllers/add_newPackage.php
+++ Application/powerman/application/controllers/add_newPackage.php
@@ -4,15 +4,23 @@ class add_newPackage extends CI_Controller
 {
 	function index()
 	{
+	
+	
+		$this->load->model('add_newPackage_model');
+
+		$new_packages = $this->add_newPackage_model->add_package();
+		$data['packages'] = $new_packages;
 		if(!($this->session->userdata('is_logged_in')))
 		{
-			$this->load->view('access_denied');
+			//$this->load->view('access_denied');
+			$this->load->view('add_newPackage');
 		}
 		else 
 		{
-			$this->load->view('add_newPackage');
+			$this->load->view('add_newPackage',$data);
 		}
 	}
+	
 	
 	
 }
