b5879cdfb8a36c208f2769cee347504faa382350
kosalakoshi88@gmail.com <kosalakoshi88@gmail.com@cf12d2a9-c71b-8214-24f5-556040d79573>
2013-09-07 04:18:39 +0000

    2013-09-7,morning,Koshala,database connection

--- trunk/Application/powerman/application/controllers/Upgrade_Package.php
+++ trunk/Application/powerman/application/controllers/Upgrade_Package.php
@@ -4,6 +4,11 @@ class Upgrade_Package extends CI_Controller
 {
 	function index()
 	{
+		$this->load->model('upgrade_package_model');
+
+		$new_packages = $this->upgrade_package_model->add_package();
+		
+		$data['packages'] = $new_packages;
 		if(!($this->session->userdata('is_logged_in')))
 		{
 			$this->load->view('access_denied');
