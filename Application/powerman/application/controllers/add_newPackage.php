893e67d58238a35de6f8fe89333f92081e805c69
mksnisrina <mksnisrina@gmail.com>
2013-09-13 08:16:55 +0000

    2013-09-13-morning-add_newPackageForm_corrected-nisrina

--- Application/powerman/application/controllers/add_newPackage.php
+++ Application/powerman/application/controllers/add_newPackage.php
@@ -29,10 +29,10 @@ class add_newPackage extends CI_Controller
 		
 		$this->form_validation->set_rules('nPackage','Package Name','trim|required');
 		$this->form_validation->set_rules('details','Description','trim|required');
-		$this->form_validation->set_rules('mDevices','Main Devices','trim|required');
-		$this->form_validation->set_rules('sDevices','Sub devices','trim|required');
-		$this->form_validation->set_rules('smsAmount','SMS Amount','trim|required');
-		$this->form_validation->set_rules('duration','Duration','trim|required');
+		$this->form_validation->set_rules('mDevices','Main Devices','trim|required|numeric|max_length[11]');
+		$this->form_validation->set_rules('sDevices','Sub devices','trim|required|numeric|max_length[11]');
+		$this->form_validation->set_rules('smsAmount','SMS Amount','trim|required|numeric|max_length[11]');
+		$this->form_validation->set_rules('duration','Duration','trim|required|numeric|max_length[11]');
 		$this->form_validation->set_rules('eDuration"','Expire duration','regex_match[(0[1-9]|1[0-9]|2[0-9]|3(0|1))-(0[1-9]|1[0-2])-\d{4}]');
 		
 		if($this->form_validation->run() == FALSE)
--- Application/powerman/application/views/includes/head.php
+++ Application/powerman/application/views/includes/head.php
@@ -57,7 +57,7 @@
 		<script src="<?php echo base_url();?>js/custom.js"></script>
         <script src="<?php echo base_url();?>js/custom-login-form.js"></script>
         <script src="<?php echo base_url();?>js/upgrade-form.js"></script>
-       <!-- <script src="<?php echo base_url();?>js/add_newPackage_form.js"></script>-->
+        <script src="<?php echo base_url();?>js/add_newPackage_form.js"></script>
       <script src="<?php echo base_url();?>js/highcharts.js"></script>
 	
 		
