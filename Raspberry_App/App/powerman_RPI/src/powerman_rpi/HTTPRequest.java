3ded721f08d1d7bef630bdc82fbe39cd49adb879
hasitha.hpmax <hasitha.hpmax@gmail.com>
2013-09-22 13:31:43 +0000

    2013-09-22 Evening 1 - New updates for Rasberry Pi App

--- Raspberry_App/App/powerman_RPI/src/powerman_rpi/HTTPRequest.java
+++ Raspberry_App/App/powerman_RPI/src/powerman_rpi/HTTPRequest.java
@@ -6,6 +6,8 @@ package powerman_rpi;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,51 +17,225 @@ import java.util.logging.Logger;
  */
 public class HTTPRequest {
     
-    public static void main(String[] args) throws Exception {
+    public static void controlDevice(DBConnector db_con) throws Exception {
         
-        new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                while(true)
+        try 
+        {
+            
+            Boolean breaker = true;
+            int skipper = 0;
+            
+            ResultSet rs1  =  db_con.search("SELECT id,mainDevice_id,device_id,control_status FROM power_control_log WHERE check_status = '0'");
+            
+            while(rs1.next())
+            {
+                ResultSet rs2 = db_con.search("SELECT ip FROM power_device_map WHERE device_id = '"+ rs1.getString("mainDevice_id") +"'");
+                
+                String device_ip = rs2.getString("ip");
+                
+                ResultSet rs3 = db_con.search("SELECT seq FROM power_subdevice_control WHERE mainDevice_id = '"+ rs1.getString("mainDevice_id") +"' AND device_id = '"+ rs1.getString("device_id") +"'");
+                
+                String device_seq = rs3.getString("seq");
+                
+                String tmp_cstatus = "0";
+                
+                if(rs1.getString("control_status").equals("0"))
                 {
-                    try {
-                        try {
-                            sendGet();
-                        } catch (Exception ex) {
-                            Logger.getLogger(HTTPRequest.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                        Thread.sleep(1000);
-                    } catch (InterruptedException ex) {
-                        Logger.getLogger(HTTPRequest.class.getName()).log(Level.SEVERE, null, ex);
+                    tmp_cstatus = "off";
+                }
+                else
+                {
+                    if(rs1.getString("control_status").equals("1"))
+                    {
+                        tmp_cstatus = "on";
                     }
                 }
+                
+                while(breaker)
+                {
+
+                    String USER_AGENT = "Mozilla/5.0";
+                    
+                    String url = "http://" + device_ip + "/setsocket.xml?num=" + device_seq + "&set=" + tmp_cstatus + "&ran=" + generateRandom();
+                    URL obj = new URL(url);
+                    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+                    // optional default is GET
+                    con.setRequestMethod("GET");
+                    //add request header
+                    con.setRequestProperty("User-Agent", USER_AGENT);
+                    int responseCode = con.getResponseCode();
+                    System.out.println("\nSending 'GET' request to URL : " + url);
+                    System.out.println("Response Code : " + responseCode);
+
+                    if(responseCode == 200)
+                    {
+                        breaker = false;
+                        db_con.change("UPDATE power_schedule_log SET check_status = '1' WHERE id = '"+ rs1.getString("id") +"'");
+                    }
+                    skipper++;
+
+                    if(skipper == 3)
+                    {
+                        breaker = false;
+                    }
+
+                }
             }
-        }).start();
-        
-        
-    }
-    
-    private static void sendGet() throws Exception {
-        
-        try {
-            
-            String USER_AGENT = "Mozilla/5.0";
-        
-            String url = "http://www.google.com/search?q=mkyong";
-            URL obj = new URL(url);
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-            // optional default is GET
-            con.setRequestMethod("GET");
-            //add request header
-            con.setRequestProperty("User-Agent", USER_AGENT);
-            int responseCode = con.getResponseCode();
-            System.out.println("\nSending 'GET' request to URL : " + url);
-            System.out.println("Response Code : " + responseCode);
-            
+                
         } catch (Exception e) {
             System.out.println("Connection Lost.");
         }
 
     }
+    
+    public static void scheduleDevice(DBConnector db_con) throws Exception {
+        
+        try 
+        {
+            
+            Boolean breaker = true;
+            int skipper = 0;
+            
+            ResultSet rs1  =  db_con.search("SELECT id,mainDevice_id,device_id,control_status FROM `power_schedule_log` WHERE DATE(schedule_on) <= DATE(NOW()) AND check_status = '0'");
+            
+            while(rs1.next())
+            {
+                ResultSet rs2 = db_con.search("SELECT ip FROM power_device_map WHERE device_id = '"+ rs1.getString("mainDevice_id") +"'");
+                
+                String device_ip = rs2.getString("ip");
+                
+                ResultSet rs3 = db_con.search("SELECT seq FROM power_subdevice_control WHERE mainDevice_id = '"+ rs1.getString("mainDevice_id") +"' AND device_id = '"+ rs1.getString("device_id") +"'");
+                
+                String device_seq = rs3.getString("seq");
+                
+                String tmp_cstatus = "0";
+                
+                if(rs1.getString("control_status").equals("0"))
+                {
+                    tmp_cstatus = "off";
+                }
+                else
+                {
+                    if(rs1.getString("control_status").equals("1"))
+                    {
+                        tmp_cstatus = "on";
+                    }
+                }
+                
+                while(breaker)
+                {
+
+                    String USER_AGENT = "Mozilla/5.0";
+                    
+                    String url = "http://" + device_ip + "/setsocket.xml?num=" + device_seq + "&set=" + tmp_cstatus + "&ran=" + generateRandom();
+                    URL obj = new URL(url);
+                    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+                    // optional default is GET
+                    con.setRequestMethod("GET");
+                    //add request header
+                    con.setRequestProperty("User-Agent", USER_AGENT);
+                    int responseCode = con.getResponseCode();
+                    System.out.println("\nSending 'GET' request to URL : " + url);
+                    System.out.println("Response Code : " + responseCode);
+
+                    if(responseCode == 200)
+                    {
+                        breaker = false;
+                        db_con.change("UPDATE power_schedule_log SET check_status = '1' WHERE id = '"+ rs1.getString("id") +"'");
+                    }
+                    skipper++;
+
+                    if(skipper == 3)
+                    {
+                        breaker = false;
+                    }
+
+                }
+            }
+                
+        } catch (Exception e) {
+            System.out.println("Connection Lost.");
+        }
+
+    }
+    
+    public static void pairDevice(DBConnector db_con) throws Exception {
+        
+        try 
+        {
+            
+            Boolean breaker = true;
+            int skipper = 0;
+            
+            ResultSet rs1  =  db_con.search("SELECT id,mainDevice_id,device_id,pair_status FROM power_schedule_log WHERE check_status = '0'");
+            
+            while(rs1.next())
+            {
+                ResultSet rs2 = db_con.search("SELECT ip FROM power_device_map WHERE device_id = '"+ rs1.getString("mainDevice_id") +"'");
+                
+                String device_ip = rs2.getString("ip");
+                
+                //0 >> for unpair // 1 >> for pair device
+                //Please update the links after adding
+                String url = "";
+                if(rs1.getString("pair_status").equals("0"))
+                {
+                    ResultSet rs3 = db_con.search("SELECT seq FROM power_subdevice_control WHERE mainDevice_id = '"+ rs1.getString("mainDevice_id") +"' AND device_id = '"+ rs1.getString("device_id") +"'");
+                
+                    String device_seq = rs3.getString("seq");
+                    
+                    url = "http://" + device_ip + "/setsocket.xml?num=" + device_seq + "&set=0&ran=" + generateRandom();
+                }
+                else
+                {
+                    if(rs1.getString("pair_status").equals("1"))
+                    {
+                        url = "http://" + device_ip + "/setsocket.xml?&ran=" + generateRandom();
+                    }
+                }
+                
+                while(breaker)
+                {
+
+                    String USER_AGENT = "Mozilla/5.0";
+
+                    URL obj = new URL(url);
+                    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+                    // optional default is GET
+                    con.setRequestMethod("GET");
+                    //add request header
+                    con.setRequestProperty("User-Agent", USER_AGENT);
+                    int responseCode = con.getResponseCode();
+                    System.out.println("\nSending 'GET' request to URL : " + url);
+                    System.out.println("Response Code : " + responseCode);
+
+                    if(responseCode == 200)
+                    {
+                        breaker = false;
+                        db_con.change("UPDATE power_pair_log SET check_status = '1' WHERE id = '"+ rs1.getString("id") +"'");
+                    }
+                    skipper++;
+
+                    if(skipper == 3)
+                    {
+                        breaker = false;
+                    }
+
+                }
+            }
+                
+        } catch (Exception e) {
+            System.out.println("Connection Lost.");
+        }
+
+    }
+
+    private static String generateRandom()
+    {
+        String val = String.valueOf((int) (Math.random()*4689));
+        
+        return val;
+    }
+    
+    
     }
