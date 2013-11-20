173a436c19c764e8246cb0ecc0cc704b0dd2fe4e
hasitha.hpmax <hasitha.hpmax@gmail.com>
2013-11-20 20:52:57 +0000

    2013-11-21 Morning 1 - Malfunction and Exceed Time Alert and SMS generation Complete

--- Corn/corn_tabs/config.php
+++ Corn/corn_tabs/config.php
@@ -7,3 +7,5 @@ define('DBNAME','powerman');
 define('TIMEOUT',40);
 
 define('SCODE','2702cb5b964b0962ac590707152d234b');
+
+define('SDIGNORE',65);
--- Corn/corn_tabs/minute_corn.php
+++ Corn/corn_tabs/minute_corn.php
@@ -5,7 +5,7 @@ include dirname(__FILE__).'/config.php';
 	$conn = mysql_connect(DBHOST, DBUSER, DBPASS);
 	if(! $conn )
 	{
-		die('Could not connect: ' . mysql_error());
+		die('Could not connect14: ' . mysql_error());
 	}
 	mysql_select_db(DBNAME);
 	
@@ -37,7 +37,7 @@ include dirname(__FILE__).'/config.php';
 
 	if(! $retval1 )
 	{
-		die('Could not get data: ' . mysql_error());
+		die('Could not get data15: ' . mysql_error());
 	}
 				
 	while($row1 = mysql_fetch_array($retval1, MYSQL_ASSOC))
@@ -48,7 +48,7 @@ include dirname(__FILE__).'/config.php';
 		
 		if(! $retval2 )
 		{
-			die('Could not get data: ' . mysql_error());
+			die('Could not get data16: ' . mysql_error());
 		}
 		
 		while($row2 = mysql_fetch_array($retval2, MYSQL_ASSOC))
@@ -60,7 +60,7 @@ include dirname(__FILE__).'/config.php';
 			
 			if(! $retval3 )
 			{
-				die('Could not get data: ' . mysql_error());
+				die('Could not get data17: ' . mysql_error());
 			}
 			
 			$query_splm = "INSERT INTO power_spl_minute (pcon,log_on,device_id,maindevice_id,created_on) VALUES ";
@@ -75,11 +75,12 @@ include dirname(__FILE__).'/config.php';
 				
 				/*-----Function's Start Call - Malfucntion, -----*/
 				
-				malfunction_alert_generator($row3['totalpcon'],$row2['device_id'],$row1['device_id'],$row2['device_type'],$current_logtime,$conn);
-				
+				malfunction_alert_generator($row3['totalpcon'],$row2['device_id'],$row1['device_id'],$row2['device_type'],$current_logtime,$row3['new_logon'],$conn);
+
 				/*-----Function's Stop Call - Malfucntion, -----*/
+				
 			}
-			
+
 			$query_splm = rtrim($query_splm,',');
 			//echo $query_splm . "<br><hr><br>";
 			if($has_pconrows)
@@ -88,10 +89,9 @@ include dirname(__FILE__).'/config.php';
 
 				if(! $retval4 )
 				{
-				  die('Could not enter data: ' . mysql_error());
+				  die('Could not enter data18: ' . mysql_error());
 				}
-				
-				if($retval4)
+				else
 				{
 					$query_splm_ack = "UPDATE power_subdevice_powerlog SET check_status = '1' WHERE device_id = '" . $row2['device_id'] . "' AND maindevice_id = '" . $row1['device_id'] . "' AND log_on < '" . $main_time . "'";
 					
@@ -99,7 +99,7 @@ include dirname(__FILE__).'/config.php';
 	
 					if(! $retval5 )
 					{
-					  die('Could not enter data: ' . mysql_error());
+					  die('Could not enter data19: ' . mysql_error());
 					}
 					
 				}
@@ -116,7 +116,7 @@ include dirname(__FILE__).'/config.php';
 	
 	//-----Malfuction Detect Method--------START
 	
-	function malfunction_alert_generator($pcon,$device_id,$maindevice_id,$device_type,$alert_on,$conn)
+	function malfunction_alert_generator($pcon,$device_id,$maindevice_id,$device_type,$alert_on,$mainlog_on,$conn)
 	{
 		$sql10 = "SELECT * FROM power_define_device WHERE id = (SELECT device_type FROM power_user_subdevices WHERE device_id = '". $device_id ."' AND maindevice_id = '". $maindevice_id ."')";
 		
@@ -124,7 +124,7 @@ include dirname(__FILE__).'/config.php';
 		
 		if(! $retval10 )
 		{
-			die('Could not get data: ' . mysql_error());
+			die('Could not get data20: ' . mysql_error());
 		}
 		
 		while($row10 = mysql_fetch_array($retval10, MYSQL_ASSOC))
@@ -140,7 +140,7 @@ include dirname(__FILE__).'/config.php';
 	
 				if(! $retval11 )
 				{
-					die('Could not enter data: ' . mysql_error());
+					die('Could not enter data21: ' . mysql_error());
 				}
 				else 
 				{
@@ -150,7 +150,7 @@ include dirname(__FILE__).'/config.php';
 					
 					if(! $retval12 )
 					{
-						die('Could not get data: ' . mysql_error());
+						die('Could not get data22: ' . mysql_error());
 					}
 					
 					while($row12 = mysql_fetch_array($retval12, MYSQL_ASSOC))
@@ -174,7 +174,7 @@ include dirname(__FILE__).'/config.php';
 						
 						if(! $retval13 )
 						{
-							die('Could not get data: ' . mysql_error());
+							die('Could not get data23: ' . mysql_error());
 						}
 						
 						while($row13 = mysql_fetch_array($retval13, MYSQL_ASSOC))
@@ -187,11 +187,11 @@ include dirname(__FILE__).'/config.php';
 						//-----Get Main Device Location Data--------START
 						$sql14 = "SELECT * FROM power_location WHERE id = '". $tmp_alert_location_id ."'";
 
-						$retval14 = mysql_query( $sql13, $conn );
+						$retval14 = mysql_query( $sql14, $conn );
 						
 						if(! $retval14 )
 						{
-							die('Could not get data: ' . mysql_error());
+							die('Could not get data24: ' . mysql_error());
 						}
 						
 						while($row14 = mysql_fetch_array($retval14, MYSQL_ASSOC))
@@ -208,7 +208,7 @@ include dirname(__FILE__).'/config.php';
 						
 						if(! $retval15 )
 						{
-							die('Could not get data: ' . mysql_error());
+							die('Could not get data25: ' . mysql_error());
 						}
 						
 						while($row15 = mysql_fetch_array($retval15, MYSQL_ASSOC))
@@ -243,10 +243,16 @@ include dirname(__FILE__).'/config.php';
 							
 							/*--------Malfunction SMS Send - END--------*/
 							
+							/*-------Time Exceed Alert - START-------*/
+							
+							timeexceed_alert_generator($pcon,$maindevice_id,$device_id,$mainlog_on,$tmp_device_detail,$conn);
+							
+							/*-------Time Exceed Alert - END-------*/
+							
 						
 						if(! $retval16 )
 						{
-						  die('Could not enter data: ' . mysql_error());
+						  die('Could not enter data1: ' . mysql_error());
 						}
 						else
 						{
@@ -257,7 +263,7 @@ include dirname(__FILE__).'/config.php';
 		
 							if(! $retval17 )
 							{
-							  die('Could not enter data: ' . mysql_error());
+							  die('Could not enter data2: ' . mysql_error());
 							}
 							/*---Delete temp row---Start--*/
 						}
@@ -282,7 +288,7 @@ include dirname(__FILE__).'/config.php';
 					
 		if(! $retval18 )
 		{
-			die('Could not get data: ' . mysql_error());
+			die('Could not get data3: ' . mysql_error());
 		}
 					
 		while($row18 = mysql_fetch_array($retval18, MYSQL_ASSOC))
@@ -295,7 +301,7 @@ include dirname(__FILE__).'/config.php';
 	
 				if(! $retval19 )
 				{
-					die('Could not enter data: ' . mysql_error());
+					die('Could not enter data4: ' . mysql_error());
 				}
 				
 				while($row19 = mysql_fetch_array($retval19, MYSQL_ASSOC))
@@ -306,7 +312,7 @@ include dirname(__FILE__).'/config.php';
 		
 					if(! $retval20 )
 					{
-						die('Could not enter data: ' . mysql_error());
+						die('Could not enter data5: ' . mysql_error());
 					}
 				}				
 			}
@@ -327,7 +333,7 @@ include dirname(__FILE__).'/config.php';
 					
 		if(! $retval21 )
 		{
-			die('Could not get data: ' . mysql_error());
+			die('Could not get data6: ' . mysql_error());
 		}
 					
 		while($row21 = mysql_fetch_array($retval21, MYSQL_ASSOC))
@@ -340,7 +346,7 @@ include dirname(__FILE__).'/config.php';
 	
 				if(! $retval22 )
 				{
-					die('Could not enter data: ' . mysql_error());
+					die('Could not enter data7: ' . mysql_error());
 				}
 				
 				while($row22 = mysql_fetch_array($retval22, MYSQL_ASSOC))
@@ -353,7 +359,7 @@ include dirname(__FILE__).'/config.php';
 		
 					if(! $retval23 )
 					{
-						die('Could not enter data: ' . mysql_error());
+						die('Could not enter data8: ' . mysql_error());
 					}
 				}				
 			}
@@ -361,6 +367,91 @@ include dirname(__FILE__).'/config.php';
 	}
 	
 	//-----Malfuction Sugestions Method--------END
+	
+	//-----Maximum Time Period Exceed Alert--------START
+	
+	function timeexceed_alert_generator($pcon,$maindevice_id,$device_id,$log_on,$device_info,$conn)
+	{
+		if($pcon > SDIGNORE)
+		{		
+			$sql24 = "SELECT TIMESTAMPDIFF(SECOND,(SELECT log_on FROM power_spl_minute WHERE log_on < '". $log_on ."' AND pcon < '". SDIGNORE ."' AND device_id = '". $device_id ."' AND maindevice_id  = '". $maindevice_id ."' ORDER BY log_on DESC LIMIT 1),'". $log_on ."') AS diff_time";
+							
+			$retval24 = mysql_query( $sql24, $conn );
+		
+			if(! $retval24 )
+			{
+				die('Could not enter data9: ' . mysql_error());
+			}
+					
+			while($row24 = mysql_fetch_array($retval24, MYSQL_ASSOC))
+			{
+				$tmp_time_diff = $row24['diff_time'];
+				
+				$tmp_alert_string = "This device exceed it normal execution time. Currently it worked ". gmdate("H:i:s", $tmp_time_diff) ."Hours. Device Info : ". $device_info;
+				
+				$sql25 = "INSERT INTO power_alert_main(device_id,maindevice_id,alert_discri,alert_priority,alert_type_id,created_by,created_on) VALUES ('". $device_id ."','". $maindevice_id ."','". $tmp_alert_string . "','High','3','1000','". $log_on ."')";
+						
+				$retval25 = mysql_query( $sql25, $conn );
+				
+				if(! $retval25 )
+				{
+					die('Could not enter data10: ' . mysql_error());
+				}
+				else 
+				{
+					timeexceed_alert_sms($maindevice_id, $tmp_alert_string, $log_on, $conn);
+				}
+				
+			}
+		}
+		
+	}
+	
+	//-----Maximum Time Period Exceed Alert--------END
+	
+	//-----Maximum Time Period Exceed Alert SMS--------START
+	
+	function timeexceed_alert_sms($maindevice_id,$message,$send_on,$conn)
+	{
+		$sql26 = "SELECT time_warn_sms,user_id FROM power_alert_config WHERE user_id = (SELECT power_users_id FROM power_user_device WHERE device_id = '". $maindevice_id ."')";
+
+		$retval26 = mysql_query( $sql26, $conn );
+					
+		if(! $retval26 )
+		{
+			die('Could not get data11: ' . mysql_error());
+		}
+					
+		while($row26 = mysql_fetch_array($retval26, MYSQL_ASSOC))
+		{
+			if($row26['time_warn_sms'] == 1)
+			{
+				$sql27 = "SELECT phoneno FROM power_users WHERE id = '". $row26['user_id'] ."'";
+						
+				$retval27 = mysql_query( $sql27, $conn );
+	
+				if(! $retval27 )
+				{
+					die('Could not enter data12: ' . mysql_error());
+				}
+				
+				while($row27 = mysql_fetch_array($retval27, MYSQL_ASSOC))
+				{
+					$sql28 = "INSERT INTO power_sms_send (message,phone_no,created_by,created_on) VALUES ('". $message ."','". $row27['phoneno'] ."','1000','". $send_on ."')";
+					
+					$retval28 = mysql_query( $sql28, $conn );
+		
+					if(! $retval28 )
+					{
+						die('Could not enter data13: ' . mysql_error());
+					}
+				}				
+			}
+		}
+	}
+
+	
+	//-----Maximum Time Period Exceed Alert SMS--------END
 	
 	/*-------------------Other Function-STOP--------------------*/	
 	
