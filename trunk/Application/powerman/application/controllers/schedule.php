4589758600946093b58136eb83c888c825adb383
hasitha.hpmax@gmail.com <hasitha.hpmax@gmail.com@cf12d2a9-c71b-8214-24f5-556040d79573>
2013-11-24 11:43:27 +0000

    2013-11-24  Evening 3 - New Database Backup

--- trunk/Application/powerman/application/controllers/schedule.php
+++ trunk/Application/powerman/application/controllers/schedule.php
@@ -2,25 +2,27 @@
 
 class schedule extends CI_Controller
 {
-	function index()
+	function index($device_id,$maindevice_id)
 	{
 	
-		
-
 		if(!($this->session->userdata('is_logged_in')))
 		{
-
 			$this->load->view('access_denied');
 			//$this->load->view('add_newPackage');
-
 		}
 		else 
 		{
-
-			$this->load->view('schedule'); 
+			$this->load->model('schedule_model');
+			$result = $this->schedule_model->get_schedules($device_id,$maindevice_id);	
+			
+			$data = array('content'=>$result,'user_roles_arr'=>$this->session->userdata('user_roles'));
+			
+			$this->load->view('schedule',$data); 
 			//$this->load->view('access_denied');
-
 		}
+		
+		
+		
 		
 				
 	}
